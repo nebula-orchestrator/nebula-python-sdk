31b71b313c50d00b4ab524b8f900f23d79841a85
naorlivne <naorlivne@gmail.com>
2017-06-12 15:48:13 +0300

    adding setup.py

---
+++ NebulaPythonSDK/__init__.py
@@ -0,0 +1,3 @@
+__author__ = 'Naor Livne'
+
+from .sdk import Nebula
---
+++ setup.py
@@ -0,0 +1,26 @@
+__author__ = 'Naor Livne'
+__author_email__ = 'naorlivne@gmail.com'
+__version__ = '0.7'
+
+from setuptools import setup, find_packages
+
+setup(name='NebulaPythonSDK',
+      author=__author__,
+      author_email=__author_email__,
+      version=__version__,
+      description="NebulaPythonSDK is a Pythonic SDK to manage Nebula container orchestrator,",
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+      scripts=['ez_setup.py', 'setup.py'],
+      license="GPLv3",
+      keywords="nebula container orchestrator sdk",
+      url="https://github.com/nebula-orchestrator/nebula-python-sdk",
+      install_requires=['requests>=2.3.0', 'json', 'base64'],
+      classifiers=["Development Status :: 4 - Beta",
+                   "Environment :: Other Environment",
+                   "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                   "Operating System :: OS Independent",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: System Administrators",
+                   "Intended Audience :: DevOps",
+                   "Topic :: Internet :: WWW/HTTP",
+                   "Topic :: Software Development :: Libraries :: Python Modules"])
