181b0ed4be5becd3e184a62a54235b03ce89f1cc
Naor Livne <naor@mintigo.com>
2018-07-26 17:14:04 +0300

    adding timeout to all requests sent from the SDK

--- NebulaPythonSDK/sdk.py
+++ NebulaPythonSDK/sdk.py
@@ -23,49 +23,49 @@ class Nebula:
         url = self.host + "/api/apps/" + app
         payload = json.dumps(config)
         headers = self.headers
-        response = requests.request("POST", url, data=payload, headers=headers)
+        response = requests.request("POST", url, data=payload, headers=headers, timeout=300)
         return response
 
     # delete an existing nebula app, no confirmation required in SDK so be careful
     def delete_app(self, app):
         url = self.host + "/api/apps/" + app
         headers = self.headers
-        response = requests.request("DELETE", url, headers=headers)
+        response = requests.request("DELETE", url, headers=headers, timeout=300)
         return response
 
     # list all of the apps managed by nebula
     def list_apps(self):
         url = self.host + "/api/apps"
         headers = self.headers
-        response = requests.request("GET", url, headers=headers)
+        response = requests.request("GET", url, headers=headers, timeout=300)
         return response
 
     # list the config of a nebula app, only requires the app name
     def list_app_info(self, app):
         url = self.host + "/api/apps/" + app
         headers = self.headers
-        response = requests.request("GET", url, headers=headers)
+        response = requests.request("GET", url, headers=headers, timeout=300)
         return response
 
     # stop a nebula app, only requires the app name
     def stop_app(self, app):
         url = self.host + "/api/apps/" + app + "/stop"
         headers = self.headers
-        response = requests.request("POST", url, headers=headers)
+        response = requests.request("POST", url, headers=headers, timeout=300)
         return response
 
     # start a nebula app, only requires the app name
     def start_app(self, app):
         url = self.host + "/api/apps/" + app + "/start"
         headers = self.headers
-        response = requests.request("POST", url, headers=headers)
+        response = requests.request("POST", url, headers=headers, timeout=300)
         return response
 
     # restart a nebula app, only requires the app name
     def restart_app(self, app):
         url = self.host + "/api/apps/" + app + "/restart"
         headers = self.headers
-        response = requests.request("POST", url, headers=headers)
+        response = requests.request("POST", url, headers=headers, timeout=300)
         return response
 
     # update a nebula app, requires the app name and a dict of the config values you want to change, any combination of
@@ -74,19 +74,19 @@ class Nebula:
         url = self.host + "/api/apps/" + app + "/update"
         payload = json.dumps(config)
         headers = self.headers
-        response = requests.request("PUT", url, data=payload, headers=headers)
+        response = requests.request("PUT", url, data=payload, headers=headers, timeout=300)
         return response
 
     # rolling restart an app, only requires the app name
     def roll_app(self, app):
         url = self.host + "/api/apps/" + app + "/roll"
         headers = self.headers
-        response = requests.request("POST", url, headers=headers)
+        response = requests.request("POST", url, headers=headers, timeout=300)
         return response
 
     # check that the contacted api is responding as expected
     def check_api(self):
         url = self.host + "/api/status"
         headers = self.headers
-        response = requests.request("GET", url, headers=headers)
+        response = requests.request("GET", url, headers=headers, timeout=300)
         return response
