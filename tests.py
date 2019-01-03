ec854f22e1cc7b27c9541e4043ece71bde58191c
Naor Livne <naor@mintigo.com>
2019-01-03 11:32:33 +0200

    filtering the response to a dict of only the values that might really be used

--- tests.py
+++ tests.py
@@ -1,7 +1,23 @@
 from unittest import TestCase
+import os
+from NebulaPythonSDK.sdk import Nebula
+
+def nebula_connection():
+    nebula_user = os.getenv("USER")
+    nebula_password = os.getenv("PASSWORD")
+    nebula_hostname = os.getenv("HOST", "127.0.0.1")
+    nebula_port = int(os.getenv("PORT", "80"))
+    nebula_protocol = os.getenv("PROTOCOL", "http")
+    nebula_request_timeout = int(os.getenv("REQUEST_TIMEOUT", "60"))
+    connection = Nebula(username=nebula_user, password=nebula_password, host=nebula_hostname, port=nebula_port,
+                        protocol=nebula_protocol, request_timeout=nebula_request_timeout)
+    return connection
 
 
 class BaseTests(TestCase):
 
-    def test_x(self):
-        pass
+    def test_check_api(self):
+        nebula_connection_object = nebula_connection()
+        reply = nebula_connection_object.check_api()
+        if reply["status_code"] != 200:
+            raise Exception("check /status failed")
